55c3559b678e506ed798fd6d0d9b4724345b3a6e
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-10-30 15:20:25 +0100

    feat(ex00): add initial implementation of Zombie class and Makefile
    Introduce a Zombie class with basic functionality, including
    constructor, destructor, and announce method. Implement main
    function to demonstrate heap and stack allocation of Zombie
    objects. Add a Makefile to manage build process, ensuring
    consistent compilation and linking of source files.

---
+++ ex00/src/Zombie.cpp
@@ -0,0 +1,17 @@
+#include "Zombie.hpp"
+#include <iostream>
+
+Zombie::Zombie(std::string name) : _name(name)
+{
+	std::cout << "A new zombie named " << this->_name << " is born" << std::endl;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << this->_name << " is dead" << std::endl;
+}
+
+void Zombie::announce(void) const
+{
+	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ ex00/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Zombie.hpp"
+
+Zombie* newZombie(std::string name);
+void randomChump ( std::string name );
+
+int	main(void)
+{
+	std::cout << "Creating a heap allocated Zombie named Foo" << std::endl;
+	Zombie* z = newZombie("Foo");
+	z->announce();
+	delete z;
+	std::cout << "\nCreating a stack allocated Zombie named Bar" << std::endl;
+	randomChump("Bar");
+	return 0;
+}
